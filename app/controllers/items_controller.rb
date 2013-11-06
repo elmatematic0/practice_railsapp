fbb0b96808907e97c5dc13858e270aaaf1b2777f
joshisaurabh <reachme15@yahoo.com>
2013-11-06 18:36:59 -0500

    we enabaled the delete function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,12 @@ class ItemsController < ApplicationController
     end        
   end
 
-  # def destroy
-  # end
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to items_path
+
+  end
 
 private
   def item_params
