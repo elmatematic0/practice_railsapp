5f827f502af200bbb17b7d169b116645bc65e336
elmatematic0 <wkdalebout+github@gmail.com>
2013-11-06 10:00:44 -0500

    adding validation to share items form. Learned that the Item class (found in the models/item.rb) creates an instance of item in the database. If the class Item does not exist, throws up constant item error.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ActiveRecord::Base
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :email, presence: true
+  validates :title, length: { minimum: 5, maximum: 25 }            
+  validates :description, length: { minimum: 5, maximum: 100 }
+  # validates_format_of   :email
+  #                       :with       => /^.+@.+$/,
+  #                       :message    => 'email must be valid'
+
+  
 end
